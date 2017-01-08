6457b4de95b8af3a1e88b42a6be494fa582c158c
James Talbert <jtalbert123@gmail.com>
2017-01-07 18:46:06 -0600

    I'm trying Unity, it has less vector graphics support (unless I want to essentially bypass their graphics engine) but has much more effecient graphics than winforms.

---
+++ Unity Project/Assets/InitInverted2DEdges.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitInverted2DEdges : MonoBehaviour {
+
+    public int NumEdges;
+    public float Radius;
+
+    // Use this for initialization
+    void Start () {
+        EdgeCollider2D edgeCollider = GetComponent<EdgeCollider2D>();
+        Vector2[] points = new Vector2[NumEdges+1];
+
+        for (int i = 0; i <= NumEdges; i++)
+        {
+            float angle = 2 * Mathf.PI * i / NumEdges;
+            float x = Radius * Mathf.Cos(angle);
+            float y = Radius * Mathf.Sin(angle);
+
+            points[i] = new Vector2(x, y);
+        }
+        edgeCollider.points = points;
+    }
+}
---
+++ Unity Project/Assets/MouseControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour {
+    public float forceFactor = 1;
+    public float maxSpeed = 1;
+    Rigidbody2D body;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        if (body.velocity.magnitude > maxSpeed)
+        {
+            if ((x > 0 && body.velocity.x > 0) ||
+                (x < 0 && body.velocity.x < 0))
+            { // Same Direction in X
+                x = 0;
+            }
+            if ((y > 0 && body.velocity.y > 0) ||
+                (y < 0 && body.velocity.y < 0))
+            { // Same Direction in Y
+                y = 0;
+            }
+        }
+        if (x != 0 || y != 0)
+        {
+            body.AddForce(new Vector2(x, y) * forceFactor);
+        }
+    }
+}
